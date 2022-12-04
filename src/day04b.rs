d7af1e91c6e1bcc00d0c9ea7bdb965cb83e45c0b
Victor W Allen <vwallen@antlab.net>
2022-12-03 22:36:25 -0800

    Day 4, HashSet version

---
+++ src/day04b.rs
@@ -0,0 +1,63 @@
+use adventofcode_2022::read_input_lines;
+use std::collections::HashSet;
+
+fn parse_range(pattern: &str) -> HashSet<u16> {
+    let mut range = HashSet::<u16>::new();
+    if let Some((a, b)) = pattern.split_once("-") {
+        (a.parse().unwrap()..=b.parse().unwrap()).for_each(|n| { range.insert(n); })
+    }
+    range    
+}
+
+pub fn prepare(file_name:&str) -> Vec<(HashSet<u16>, HashSet<u16>)> {
+    let mut input = Vec::new();
+    for line in read_input_lines(file_name).iter() {
+        if let Some((a, b)) = line.split_once(",") {
+            input.push((parse_range(a), parse_range(b)));
+        }
+    }
+    input
+}
+
+pub fn part_1(input: &Vec<(HashSet<u16>, HashSet<u16>)>) -> Option<u32> {
+    let count = input
+        .iter()
+        .filter(|(a, b)| (a & b).len() == a.len() || (a & b).len() == b.len())
+        .count();
+    Some(count as u32)
+}
+
+pub fn part_2(input: &Vec<(HashSet<u16>, HashSet<u16>)>) -> Option<u32> {
+    let count = input
+        .iter()
+        .filter(|(a, b)| (a & b).len() > 0)
+        .count();
+    Some(count as u32)
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+
+    #[test]
+    fn test_prepare() {
+        let input = prepare("day04-example.txt");
+        assert_eq!(input[0].0.contains(&2), true);
+        assert_eq!(input[0].1.contains(&8), true)
+    }
+
+    #[test]
+    fn test_part_1() {
+        let input = prepare("day04-example.txt");
+        assert_eq!(part_1(&input), Some(2))
+    }
+
+    #[test]
+    fn test_part_2() {
+        let input = prepare("day04-example.txt");
+        assert_eq!(part_2(&input), Some(4))
+    }
+
+
+}
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ pub mod day02;
 pub mod day02b;
 pub mod day03;
 pub mod day04;
+pub mod day04b;
 
 fn main() {
     println!("=== Advent of Code 2022 ====");
@@ -18,9 +19,9 @@ fn main() {
     println!("⭐ {}", day02::part_1(&input_day2).unwrap());
     println!("⭐ {}", day02::part_2(&input_day2).unwrap());
     println!("---");
-    // let input_day2b = day02b::prepare("day02.txt");
-    // println!("⭐ {}", day02b::part_1(&input_day2b).unwrap());
-    // println!("⭐ {}", day02b::part_2(&input_day2b).unwrap());
+    let input_day2b = day02b::prepare("day02.txt");
+    println!("⭐ {}", day02b::part_1(&input_day2b).unwrap());
+    println!("⭐ {}", day02b::part_2(&input_day2b).unwrap());
 
     println!("---------- Day 03 ----------");
     let input_day3 = day03::prepare("day03.txt");
@@ -31,4 +32,7 @@ fn main() {
     let input_day4 = day04::prepare("day04.txt");
     println!("⭐ {}", day04::part_1(&input_day4).unwrap());
     println!("⭐ {}", day04::part_2(&input_day4).unwrap());
+    let input_day4b = day04b::prepare("day04.txt");
+    println!("⭐ {}", day04b::part_1(&input_day4b).unwrap());
+    println!("⭐ {}", day04b::part_2(&input_day4b).unwrap());
 }
