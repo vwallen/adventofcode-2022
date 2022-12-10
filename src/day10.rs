445178531b1be3abc90139922453b8963508cca9
Victor W Allen <vwallen@antlab.net>
2022-12-10 11:57:44 -0800

    Day 10, complete

--- src/day10.rs
+++ src/day10.rs
@@ -1,6 +1,6 @@
 
-use std::collections::{VecDeque, vec_deque};
-
+use std::collections::VecDeque;
+use colored::Colorize;
 use adventofcode_2022::read_input;
 use anyhow::Result;
 
@@ -23,35 +23,65 @@ pub fn prepare(file_name: &str) -> Result<Vec<OpCode>> {
     Ok(instructions)
 }
 
-pub fn part_1(instructions: &[OpCode]) -> Option<isize> {
-    let mut signal_strengths = VecDeque::new();
-    let mut x_register:isize = 1;
-    let mut cycles:VecDeque<isize> = VecDeque::new();
-
+pub fn calculate_signal(instructions: &[OpCode]) -> Result<VecDeque<isize>> {
+    let mut signals:VecDeque<isize> = VecDeque::new();
     for op_code in instructions.iter() {
         match op_code {
-            OpCode::NoOp => { cycles.push_back(0); },
+            OpCode::NoOp => { signals.push_back(0); },
             OpCode::AddX(x) => { 
-                cycles.push_back(0);
-                cycles.push_back(*x);
+                signals.push_back(0);
+                signals.push_back(*x);
             },
         }
     }
-    for cycle in 1..=220 {
-        match cycle {
-            20|60|100|140|180|220 => { 
-                signal_strengths.push_front(cycle * x_register);
-            },
-            _ => { }
-        }
-        x_register += cycles.pop_front().unwrap();
-    }
-    let signal_strength = signal_strengths.iter().sum();
-    Some(signal_strength)
+    Ok(signals)
 }
 
-pub fn part_2(_input: &[OpCode]) -> Option<usize> {
-    None
+pub fn print_pattern(pattern: &[char], w:usize) {
+    for (i, ch) in pattern.iter().enumerate() {
+        let st = if ch == &'.' { String::from(*ch).red() } else { String::from(*ch).bright_green() };
+        if (i + 1) % w == 0 {
+                println!("{}", st);
+        } else {
+                print!("{}", st);
+        }
+    }
+}
+
+pub fn part_1(instructions: &[OpCode]) -> Option<isize> {
+    if let Ok(signals) = calculate_signal(instructions) {
+        let mut signal_strengths = VecDeque::new();
+        let mut x_register:isize = 1;
+        for (c, signal) in signals.iter().enumerate() {
+            let cycle:isize = c as isize + 1;
+            match cycle {
+                20|60|100|140|180|220 => { 
+                    signal_strengths.push_front(cycle * x_register);
+                },
+                _ => { }
+            }
+            x_register += signal;
+        }
+        let signal_strength = signal_strengths.iter().sum();
+        Some(signal_strength)
+    } else { None }
+}
+
+
+pub fn part_2(instructions: &[OpCode]) -> Option<Vec<char>> {
+    if let Ok(signals) = calculate_signal(instructions) {
+        let mut pattern = vec!['.';240];
+        let mut x_register:isize = 1;
+        for (cycle, signal) in signals.iter().enumerate() {
+            let beam_col = cycle % 40;
+            let pixl_col = x_register % 40;
+            if (beam_col as isize - pixl_col).abs() < 2 {
+                pattern.splice(cycle..=cycle, ['#']);
+            }
+            x_register += signal;
+        }
+        Some(pattern)
+    } else { None }
 }
 
 #[cfg(test)]
@@ -60,7 +90,6 @@ mod test {
     use super::*;
 
     #[test]
-    #[ignore]
     fn test_prepare() {
         // 0: addx 15
         // 1: addx -11
@@ -80,10 +109,16 @@ mod test {
     }
 
     #[test]
-    #[ignore]
     fn test_part_2() {
         if let Ok(input) = prepare("day10-example.txt") {
-            assert_eq!(part_2(&input), Some(1))
+            let test_pattern:Vec<char> = String::from("##..##..##..##..##..##..##..##..##..##..###...###...###...###...###...###...###.####....####....####....####....####....#####.....#####.....#####.....#####.....######......######......######......###########.......#######.......#######.....").chars().collect();
+            let elfs_pattern:Vec<char> = part_2(&input).unwrap();
+            println!("----------------------------------------");
+            print_pattern(&test_pattern, 40);
+            println!("----------------------------------------");
+            print_pattern(&elfs_pattern, 40);
+            println!("----------------------------------------");
+            assert_eq!(elfs_pattern, test_pattern);
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #[macro_use] extern crate scan_rules;
+use colored::Colorize;
 
 pub mod day01;
 pub mod day02;
@@ -15,42 +16,42 @@ pub mod day09;
 pub mod day10;
 
 fn main() {
-    println!("=== Advent of Code 2022 ====");
+    println!("{}", "=== Advent of Code 2022 ====".bright_red());
 
-    println!("---------- Day 01 ----------");
+    println!("{} {} {}", "----------".red(), "Day 01".bright_green(), "----------".red());
     let elves_day1 = day01::prepare("day01.txt");
     println!("⭐ {}", day01::part_1(&elves_day1).unwrap());
     println!("⭐ {}", day01::part_2(&elves_day1).unwrap());
 
-    println!("---------- Day 02 ----------");
+    println!("{} {} {}", "----------".red(), "Day 02".bright_green(), "----------".red());
     let input_day2 = day02::prepare("day02.txt");
     println!("⭐ {}", day02::part_1(&input_day2).unwrap());
     println!("⭐ {}", day02::part_2(&input_day2).unwrap());
-    println!("---");
+    println!("{}", "---".green());
     let input_day2b = day02b::prepare("day02.txt");
     println!("⭐ {}", day02b::part_1(&input_day2b).unwrap());
     println!("⭐ {}", day02b::part_2(&input_day2b).unwrap());
 
-    println!("---------- Day 03 ----------");
+    println!("{} {} {}", "----------".red(), "Day 03".bright_green(), "----------".red());
     let input_day3 = day03::prepare("day03.txt");
     println!("⭐ {}", day03::part_1(&input_day3).unwrap());
     println!("⭐ {}", day03::part_2(&input_day3).unwrap());
 
-    println!("---------- Day 04 ----------");
+    println!("{} {} {}", "----------".red(), "Day 04".bright_green(), "----------".red());
     let input_day4 = day04::prepare("day04.txt");
     println!("⭐ {}", day04::part_1(&input_day4).unwrap());
     println!("⭐ {}", day04::part_2(&input_day4).unwrap());
-    println!("---");
+    println!("{}", "---".green());
     let input_day4b = day04b::prepare("day04.txt");
     println!("⭐ {}", day04b::part_1(&input_day4b).unwrap());
     println!("⭐ {}", day04b::part_2(&input_day4b).unwrap());
-    println!("---");
+    println!("{}", "---".green());
     if let Ok(input_day4c) = day04c::prepare("day04.txt") {
         println!("⭐ {}", day04c::part_1(&input_day4c).unwrap());
         println!("⭐ {}", day04c::part_2(&input_day4c).unwrap());
     }
 
-    println!("---------- Day 05 ----------");
+    println!("{} {} {}", "----------".red(), "Day 05".bright_green(), "----------".red());
     if let Ok((mut day05_inventory, day05_commands)) = day05::prepare("day05.txt") {
         println!("⭐ {}", day05::part_1(&mut day05_inventory, day05_commands).unwrap());
     }
@@ -58,28 +59,30 @@ fn main() {
         println!("⭐ {}", day05::part_2(&mut day05_inventory, day05_commands).unwrap());
     }
 
-    println!("---------- Day 06 ----------");
+    println!("{} {} {}", "----------".red(), "Day 06".bright_green(), "----------".red());
     if let Ok(input_day06) = day06::prepare("day06.txt") {
         println!("⭐ {}", day06::part_1(&input_day06).unwrap());
         println!("⭐ {}", day06::part_2(&input_day06).unwrap());
     }
 
-    println!("---------- Day 07 ----------");
+    println!("{} {} {}", "----------".red(), "Day 07".bright_green(), "----------".red());
     println!("💀");
     println!("💀");
 
-    println!("---------- Day 08 ----------");
+    println!("{} {} {}", "----------".red(), "Day 08".bright_green(), "----------".red());
     println!("💀");
     println!("💀");
 
-    println!("---------- Day 09 ----------");
+    println!("{} {} {}", "----------".red(), "Day 09".bright_green(), "----------".red());
     println!("💀");
     println!("💀");
 
-    println!("---------- Day 10 ----------");
-    if let Ok((mut day10_input)) = day10::prepare("day10.txt") {
+    println!("{} {} {}", "----------".red(), "Day 10".bright_green(), "----------".red());
+    if let Ok(day10_input) = day10::prepare("day10.txt") {
         println!("⭐ {}", day10::part_1(&day10_input).unwrap());
-        // println!("⭐ {}", day10::part_2(&day10_input).unwrap());
+        println!("⭐");
+        let pattern = day10::part_2(&day10_input).unwrap();
+        day10::print_pattern(&pattern, 40);
     }
 
     /*
@@ -144,5 +147,5 @@ fn main() {
     println!("💀");
     println!("💀");
 */
-    println!("----------------------------")
+    println!("{}", "============================".bright_red());
 }
